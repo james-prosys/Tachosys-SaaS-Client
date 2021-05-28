3af32378a01cc6c7b98c445c8083d30d2fb35ca5
James <James@HOLLY>
2021-05-28 11:54:20 +0100

    Add project files.

---
+++ SaaSClient/Model.cs
@@ -0,0 +1,253 @@
+﻿using System;
+using System.Collections;
+
+namespace SaaSClient
+{
+    public class LoginModel
+    {
+        public string Username { get; set; }
+        public string Password { get; set; }
+    }
+
+    public class TokenModel
+    {
+        public string Token { get; set; }
+        public int ExpiresIn { get; set; }
+    }
+
+    public class FileParseResponse
+    {
+        public bool IsFileParseSuccessful { get; set; }
+        public bool IsVerifySignaturesSuccessful { get; set; }
+        public string FileParseException { get; set; }
+        public VerifySignatureResult VerifySignatureResult { get; set; }
+    }
+
+    public class VerifySignatureResult
+    {
+        public string Result { get; set; }
+        public string Exception { get; set; }
+        public SectionResult[] SectionResults { get; set; }
+    }
+
+    public class SectionResult
+    {
+        public string fileID { get; set; }
+        public int fileOccurrence { get; set; }
+        public bool success { get; set; }
+    }
+
+    public class FilenameRequest
+    {
+        public DateTime DownloadDate { get; set; }
+
+        public int NamingConvention { get; set; }
+
+    }
+
+    public class FilenameResponse
+    {
+        public string Filename { get; set; }
+    }
+
+    public class NamingConventions
+    {
+        public const int StandardEuropean = 0;
+        public const int France = 1;
+        public const int Spain = 2;
+    }
+
+    public enum Types
+    {
+        Unknown = 0,
+        DriverCard = 1,
+        WorkshopCard = 2,
+        VehicleUnit = 3
+    }
+
+    public class FileSummaryResponse
+    {
+        public Types Types { get; set; }
+        public int Generation { get; set; }
+        public DriverCardSummary DriverCardSummary { get; set; }
+        public VehicleUnitSummary VehicleUnitSummary { get; set; }
+        public WorkshopCardSummary WorkshopCardSummary { get; set; }
+    }
+
+    public class FileSectionRequest
+    {
+        public string FileID { get; set; }
+        public string RecordType { get; set; }
+        public string FileOccurrence { get; set; }
+    }
+
+    public class AnalyseRequest
+    {
+        public bool POAasBreak { get; set; }
+        public bool MissingManualEntry { get; set; }
+        public byte HomeNation { get; set; }
+        public string Language { get; set; }
+        public string TimeZone { get; set; }
+    }
+
+    public class AnalysisItem
+    {
+        public string Type { get; set; }
+        public string SubType { get; set; }
+        public string Infringement_Code { get; set; }
+        public string Infringement_Level { get; set; }
+        public DateTime DateTime { get; set; }
+        public string Message { get; set; }
+        public object[] Parameters { get; set; }
+    }
+
+    public class DriverCardSummary
+    {
+        public string CardHolderFirstNames { get; set; }
+        public string CardHolderSurname { get; set; }
+        public DateF CardHolderBirthDate { get; set; }
+        public TimeReal CardValidityBegin { get; set; }
+        public TimeReal CardExpiryDate { get; set; }
+        public string CardIssuingMemberStateAlpha { get; set; }
+        public CardNumber CardNumber { get; set; }
+        public string DrivingLicenceNumber { get; set; }
+        public ActivitySummary ActivitySummary { get; set; }
+    }
+
+    public class VehicleUnitSummary
+    {
+        public string VehicleIdentificationNumber { get; set; }
+        public string VehicleRegistrationNationAlpha { get; set; }
+        public string VehicleRegistrationNumber { get; set; }
+        public ActivitySummary ActivitySummary { get; set; }
+        public VehicleUnitIdenficationSummary VehicleUnitIdenficationSummary { get; set; }
+    }
+
+    public class WorkshopCardSummary
+    {
+        public string CardHolderFirstNames { get; set; }
+        public string CardHolderSurname { get; set; }
+        public TimeReal CardValidityBegin { get; set; }
+        public TimeReal CardExpiryDate { get; set; }
+        public string CardIssuingMemberStateAlpha { get; set; }
+        public CardNumber CardNumber { get; set; }
+        public string WorkshopName { get; set; }
+        public string WorkshopAddress { get; set; }
+    }
+
+    public class ActivitySummary
+    {
+        public TimeReal StartDate { get; set; }
+        public TimeReal EndDate { get; set; }
+    }
+
+    public class VehicleUnitIdenficationSummary
+    {
+        public string vuManufacturerName { get; set; }
+        public string vuPartNumber { get; set; }
+        public string vuSoftwareVersion { get; set; }
+    }
+
+    public class ActivityDailyRecord
+    {
+        public int activityPreviousRecordLength { get; set; }
+        public int activityRecordLength { get; set; }
+        public TimeReal activityRecordDate { get; set; }
+        public string activityDailyPresenceCounter { get; set; }
+        public int activityDayDistance { get; set; }
+
+        public ActivityChangeInfo[] ActivityChangeInfos { get; set; }
+    }
+
+    public class ActivityChangeInfo
+    {
+        public byte slot { get; set; }
+        public byte drivingStatus { get; set; }
+        public byte cardStatus { get; set; }
+        public byte activity { get; set; }
+        public TimeReal time { get; set; }
+    }
+
+    public struct PlacesRecord
+    {
+
+        public TimeReal entryTime;
+        public byte entryTypeDailyWorkPeriod;
+        public byte dailyWorkPeriodCountry;
+        public byte dailyWorkPeriodRegion;
+        public uint vehicleOdometerValue;
+
+    }
+
+    public struct SpecificConditionRecord
+    {
+        public TimeReal entryTime;
+        public byte specificConditionType;
+    }
+
+    public struct DateF
+    {
+
+        public int Year;
+        public int Month;
+        public int Day;
+
+        public override string ToString()
+        {
+            return string.Format("{0:0000}-{1:00}-{2:00}", Year, Month, Day);
+        }
+    }
+
+    public struct TimeReal
+    {
+        public UInt32 Ticks;
+
+        public static implicit operator DateTime(TimeReal t)
+        {
+            UInt32 seconds = t.Ticks;
+            UInt32 minutes = seconds / 60;
+            UInt32 remainder = seconds % 60;
+            DateTime ret = new DateTime(1970, 1, 1);
+            ret = ret.AddMinutes(minutes);
+            ret = ret.AddSeconds(remainder);
+            return ret;
+        }
+
+        public DateTime ToDateTime()
+        {
+            return Convert.ToDateTime(this);
+        }
+
+        public override string ToString()
+        {
+            if (Ticks == 0 | Ticks == UInt32.MaxValue) {
+                return "";
+            }
+            else {
+                return this.ToDateTime().ToString();
+            }
+        }
+        public string ToString(string format)
+        {
+            if (Ticks == 0 | Ticks == UInt32.MaxValue) {
+                return "";
+            }
+            else {
+                return this.ToDateTime().ToString(format);
+            }
+        }
+    }
+
+    public struct CardNumber
+    {
+
+        public string driverIdentification;
+        public string ownerIdentification;
+        public string cardConsecutiveIndex;
+        public string cardReplacementIndex;
+        public string cardRenewalIndex;
+
+    }
+}
+
+
---
+++ SaaSClient/Program.cs
@@ -0,0 +1,188 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace SaaSClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MainAsync(args).GetAwaiter().GetResult();
+
+            Console.ReadKey();
+        }
+
+        static async Task MainAsync(string[] args)
+        {
+            const string baseAddress = "https://saas.tachosys.com/api/";
+
+            var client = new HttpClient
+            {
+                BaseAddress = new Uri(baseAddress),
+                Timeout = new TimeSpan(0, 2, 0)
+            };
+            DateTime authenticationExpiry;
+
+            // Get Authentication token
+            const string tokenUri = "token";
+            const string username = "james@prosysdev.com";
+            const string password = "MrForgetful";
+
+            HttpResponseMessage response = await client.PostAsJsonAsync(tokenUri, new LoginModel
+            {
+                Username = username,
+                Password = password
+            });
+            if (response.IsSuccessStatusCode) {
+                var token = await response.Content.ReadAsAsync<TokenModel>();
+                client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", token.Token);
+                authenticationExpiry = DateTime.UtcNow.AddMinutes(token.ExpiresIn);
+                Console.WriteLine("Authentication successful.");
+            }
+            else
+                throw new ApplicationException(string.Format("Error: {0:d} {1} - {2}", response.StatusCode, response.ReasonPhrase, "Unable to authenticate with server."));
+
+            // Upload file
+            const string uploadUri = "file/upload";
+            const string filename = @"C:\Users\Public\Documents\Tachosys\digiConnect\Archive\Drivers\TESTING EXTRA GENTWO 12345678910\C_20190710_1004_E_TESTING_DB18312147040300.ddd";
+            //const string filename = @"C:\Users\Public\Documents\Tachosys\digiConnect\Archive\Vehicles\G2VDO\M_20190131_1452_G2VDO_GEN2VDO1234567890.ddd";
+
+            var form = new MultipartFormDataContent();
+            var content = new ByteArrayContent(File.ReadAllBytes(filename));
+            form.Add(content, "File", Path.GetFileName(filename));
+
+            response = await client.PostAsync(uploadUri, form);
+            if (response.IsSuccessStatusCode)
+                Console.WriteLine("File upload successful.");
+            else
+                throw new ApplicationException(string.Format("Error: {0:d} {1} - {2}", response.StatusCode, response.ReasonPhrase, "File upload failed."));
+
+            // File Parse result
+            const string parseUri = "file/parse";
+            response = await client.GetAsync(parseUri);
+            if (response.IsSuccessStatusCode) {
+                var parseResults = await response.Content.ReadAsAsync<FileParseResponse>();
+                Console.WriteLine("Parse: {0}", JsonConvert.SerializeObject(parseResults, Formatting.Indented));
+            }
+            else
+                throw new ApplicationException(string.Format("Error: {0:d} {1} - {2}", response.StatusCode, response.ReasonPhrase, "Failed to parse uploaded file."));
+
+            // Get Filename
+            const string filenameUri = "file/filename";
+
+            response = await client.PostAsJsonAsync(filenameUri, new FilenameRequest
+            {
+                DownloadDate = DateTime.UtcNow,
+                NamingConvention = NamingConventions.StandardEuropean
+            });
+            if (response.IsSuccessStatusCode)
+                Console.WriteLine("Filename: {0}", response.Content.ReadAsAsync<FilenameResponse>().Result.Filename);
+            else
+                throw new ApplicationException(string.Format("Error: {0:d} {1} - {2}", response.StatusCode, response.ReasonPhrase, "Failed to get filename."));
+
+            // Get Summary
+            const string summaryUri = "file/summary";
+
+            response = await client.GetAsync(summaryUri);
+            if (response.IsSuccessStatusCode) {
+                var summary = await response.Content.ReadAsAsync<FileSummaryResponse>();
+                Console.WriteLine("Summary: {0}", JsonConvert.SerializeObject(summary, Formatting.Indented));
+            }
+            else
+                throw new ApplicationException(string.Format("Error: {0:d} {1} - {2}", response.StatusCode, response.ReasonPhrase, "Failed to get file summary."));
+
+            // Get File section
+            const string sectionUri = "file/section";
+
+            response = await client.PostAsJsonAsync(sectionUri, new FileSectionRequest
+            {
+                FileID = "0520",
+                RecordType = "",
+                FileOccurrence = "0"
+            });
+            if (response.IsSuccessStatusCode) {
+                object section = await response.Content.ReadAsAsync<object>();
+                Console.WriteLine("File Section: {0}", JsonConvert.SerializeObject(section, Formatting.Indented));
+            }
+            else
+                throw new ApplicationException(string.Format("Error: {0:d} {1} - {2}", response.StatusCode, response.ReasonPhrase, "Failed to get file section."));
+
+            // Showing upload of ActivityInfo, Places, and SpecificCondition - analysis can be run directly against previously uploaded DriverCard
+            // These calls would be used when you have got your own ActivityInfo, Places, and SpecificCondition tables.
+            // The example below gets these sections from the uploaded DriverCard.
+
+            // Get ActivityInfo, PlacesRecords, and SpecificConditions from uploaded card file
+            ActivityDailyRecord[] activityCollection;
+            SpecificConditionRecord[] specificConditionRecords;
+            PlacesRecord[] placesRecords;
+            response = await client.PostAsJsonAsync(sectionUri, new FileSectionRequest { FileID = "0504", FileOccurrence = "0" });
+            if (response.IsSuccessStatusCode)
+                activityCollection = await response.Content.ReadAsAsync<ActivityDailyRecord[]>();
+            else
+                throw new ApplicationException(string.Format("Error: {0:d} {1} - {2}", response.StatusCode, response.ReasonPhrase, "Failed to get file section."));
+            response = await client.PostAsJsonAsync(sectionUri, new FileSectionRequest { FileID = "0506", FileOccurrence = "0" });
+            if (response.IsSuccessStatusCode)
+                placesRecords = await response.Content.ReadAsAsync<PlacesRecord[]>();
+            else
+                throw new ApplicationException(string.Format("Error: {0:d} {1} - {2}", response.StatusCode, response.ReasonPhrase, "Failed to get file section."));
+            response = await client.PostAsJsonAsync(sectionUri, new FileSectionRequest { FileID = "0522", FileOccurrence = "0" });
+            if (response.IsSuccessStatusCode)
+                specificConditionRecords = await response.Content.ReadAsAsync<SpecificConditionRecord[]>();
+            else
+                throw new ApplicationException(string.Format("Error: {0:d} {1} - {2}", response.StatusCode, response.ReasonPhrase, "Failed to get file section."));
+
+            List<ActivityChangeInfo> activity = new List<ActivityChangeInfo>();
+            foreach (ActivityDailyRecord record in activityCollection) {
+                foreach (ActivityChangeInfo change in record.ActivityChangeInfos)
+                    activity.Add(change);
+            }
+
+            // Upload ActivityInfo, PlacesRecords, and SpecificConditions
+            const string activityInfoUri = "analyse/data/activityChangeInfo";
+            const string placesRecordsUri = "analyse/data/placesRecords";
+            const string specificConditionsUri = "analyse/data/specificConditionRecords";
+
+            response = await client.PostAsJsonAsync(activityInfoUri, activity);
+            if (!response.IsSuccessStatusCode)
+                throw new ApplicationException(string.Format("Error: {0:d} {1} {2}", response.StatusCode, response.ReasonPhrase, "Failed to upload activityChangeInfo."));
+            response = await client.PostAsJsonAsync(placesRecordsUri, placesRecords);
+            if (!response.IsSuccessStatusCode)
+                throw new ApplicationException(string.Format("Error: {0:d} {1} {2}", response.StatusCode, response.ReasonPhrase, "Failed to upload placesRecords."));
+            response = await client.PostAsJsonAsync(specificConditionsUri, specificConditionRecords);
+            if (!response.IsSuccessStatusCode)
+                throw new ApplicationException(string.Format("Error: {0:d} {1} {2}", response.StatusCode, response.ReasonPhrase, "Failed to upload specificConditionRecords."));
+
+            // Get Driver Card analysis
+            const string analyseUri = "analyse";
+
+            response = await client.PostAsJsonAsync(analyseUri, new AnalyseRequest
+            {
+                POAasBreak = false,
+                MissingManualEntry = false,
+                HomeNation = 0x15,
+                Language = "en-GB",
+                TimeZone = "GMT Standard Time"
+            });
+            if (response.IsSuccessStatusCode) {
+                List<AnalysisItem> list = await response.Content.ReadAsAsync<List<AnalysisItem>>();
+                foreach (AnalysisItem item in list) {
+                    Console.WriteLine($"Type    : {item.Type}");
+                    Console.WriteLine($"SubType : {item.SubType}");
+                    Console.WriteLine($"Inf Code: {item.Infringement_Code}");
+                    Console.WriteLine($"Inf Level: {item.Infringement_Level}");
+                    Console.WriteLine($"DateTime: {item.DateTime}");
+                    Console.WriteLine($"Message : {item.Message}");
+                    Console.WriteLine();
+                }
+            }
+            else
+                throw new ApplicationException(string.Format("Error: {0:d} {1} - {2}", response.StatusCode, response.ReasonPhrase, "Unable to perform analysis."));
+
+        }
+    }
+}
